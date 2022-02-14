c81b8f9a71f908d9d5f7cf7d4d7f3f84699aa923
John <femianjc@miamioh.edu>
2022-02-14 21:48:19 +0000

    Made invert_image return latents, updated weight URLS

--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,6 +1,6 @@
 import gdown
 import os
-
+from torch.hub import download_url_to_file
 
 weight_dic = {'afhqwild.pt': 'https://drive.google.com/file/d/14OnzO4QWaAytKXVqcfWo_o2MzoR4ygnr/view?usp=sharing',
                 'afhqdog.pt': 'https://drive.google.com/file/d/16v6jPtKVlvq8rg2Sdi3-R9qZEVDgvvEA/view?usp=sharing',
@@ -9,7 +9,16 @@ weight_dic = {'afhqwild.pt': 'https://drive.google.com/file/d/14OnzO4QWaAytKXVqc
                 'metfaces.pt': 'https://drive.google.com/file/d/16wM2PwVWzaMsRgPExvRGsq6BWw_muKbf/view?usp=sharing'
 }
 
-
-def download_weight(weight_path):
+def download_weight_google(weight_path):
     gdown.download(weight_dic[os.path.basename(weight_path)],
-                   output=weight_path, fuzzy=True)
+                   output=weight_path, fuzzy=True, quiet=True)
+
+
+def url_for(weight_path):
+  return f'http://vision.csi.miamioh.edu/data/ii2s/{os.path.basename(weight_path)}'
+
+def download_weight_http(weight_path):
+    if not os.path.isfile(weight_path):
+        download_url_to_file(url_for(weight_path), weight_path, progress=True)
+
+download_weight = download_weight_http
